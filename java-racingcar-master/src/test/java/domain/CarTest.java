b5a67165c545e1213c174f0973747e337d08763f
Hyeongmin Park <thalsal@naver.com>
2022-10-25 21:31:30 +0900

    ✅ : 자동차 이름 유효성 체크

--- java-racingcar-master/src/test/java/domain/CarTest.java
+++ java-racingcar-master/src/test/java/domain/CarTest.java
@@ -1,9 +1,12 @@
 package domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.BDDMockito;
 
 public class CarTest {
@@ -12,14 +15,22 @@ public class CarTest {
     @DisplayName("자동차는 전진시 한칸씩 움직인다.")
     void car() {
         //given
-        CarMoveAbleStrategyImpl carMoveAbleStrategy = BDDMockito.mock(CarMoveAbleStrategyImpl.class);
+        CarMoveAbleStrategyImpl carMoveAbleStrategy = BDDMockito.mock(
+            CarMoveAbleStrategyImpl.class);
         BDDMockito.given(carMoveAbleStrategy.getMoveDistance()).willReturn(1);
 
         //when
-        Car car = new Car();
+        Car car = new Car(0, "name");
         car.move(carMoveAbleStrategy);
 
         //then
         assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "aaaaaaaaaaaaa"})
+    @DisplayName("자동차 이름은 빈문자열이거나, 5이상 초과하면 안됨")
+    void validName(String name) {
+        assertThatThrownBy(() -> new Car(0, name)).isInstanceOf(NullPointerException.class);
     }
 }
