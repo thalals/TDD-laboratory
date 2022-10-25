bf0b7ab0924ecab491bbab42fcad31b7151c9b72
Hyeongmin Park <thalsal@naver.com>
2022-10-25 21:29:53 +0900

    ✨️ car 이름 컴마 분리 및 우승자 추출

--- java-racingcar-master/src/main/java/domain/GameSet.java
+++ java-racingcar-master/src/main/java/domain/GameSet.java
@@ -1,7 +1,11 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
 
 
 public class GameSet {
@@ -9,15 +13,25 @@ public class GameSet {
     private final int count;
     private final List<Car> carList;
 
-    public GameSet(final int cars, final int count) {
+    public GameSet(final String cars, final int count) {
         this.count = count;
-        carList = List.copyOf(createCars(cars));
+        List<String> carsNameList = separateComma(cars).orElseThrow(
+            () -> new NullPointerException("자동차 입력을 제대로 해주세요")
+        );
+        carList = List.copyOf(createCars(carsNameList));
     }
 
-    private List<Car> createCars(final int size) {
+    private Optional<List<String>> separateComma(final String line) {
+        if (line.isBlank()) {
+            return Optional.empty();
+        }
+        return Optional.of(Arrays.stream(line.split(",")).collect(Collectors.toList()));
+    }
+
+    private List<Car> createCars(final List<String> carNames) {
         List<Car> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(new Car());
+        for (String name : carNames) {
+            list.add(new Car(0, name));
         }
         return list;
     }
@@ -26,7 +40,16 @@ public class GameSet {
         return this.count;
     }
 
-    public List<Car> getCarList(){
+    public List<Car> getCarList() {
         return this.carList;
+    }
+
+    public List<String> getWinners() {
+
+        OptionalInt maxDistance = carList.stream().mapToInt(Car::getDistance).max();
+
+        return carList.stream()
+            .filter(car -> car.isEqualsDistance(maxDistance.getAsInt()))
+            .map(Car::getName).collect(Collectors.toList());
     }
 }
